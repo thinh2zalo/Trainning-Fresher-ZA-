4fb402a56dcf56dc504d925c1be444331b8ea320
Thinh <hoaithipm2@gmail.com>
2019-08-09 13:36:43 +0700

    handle content of file

---
+++ bacsicObjC/bacsicObjC/HandleContentFile.h
@@ -0,0 +1,19 @@
+//
+//  NSFileManager+HandleContentFile.h
+//  ProjectFresherZAMD
+//
+//  Created by CPU11606 on 8/8/19.
+//  Copyright © 2019 CPU11606. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HandleContentFile :NSObject
+
+-(void)splitFileWithURL:(NSURL *)urlInput andN:(NSUInteger)N;
+-(void)splitFileWithURLAndNumberSizeInput:(NSURL *)urlInput andNumberOfByte:(NSUInteger)NByte ;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ bacsicObjC/bacsicObjC/HandleContentFile.m
@@ -0,0 +1,122 @@
+//
+//  NSFileManager+HandleContentFile.m
+//  ProjectFresherZAMD
+//
+//  Created by CPU11606 on 8/8/19.
+//  Copyright © 2019 CPU11606. All rights reserved.
+//
+
+#import "HandleContentFile.h"
+
+
+
+@implementation HandleContentFile 
+
+-(void)splitFileWithURLAndNumberSizeInput:(NSURL *)urlInput andNumberOfByte:(NSUInteger)NByte {
+    NSFileHandle *inputFileHandle;
+    NSFileHandle *outputFileHandle;
+    NSFileManager * fm = [NSFileManager defaultManager];
+    NSString *directoryPath = urlInput.path;
+    NSLog(@"%@",directoryPath);
+    
+    if (![fm fileExistsAtPath:directoryPath]){
+        NSLog(@"Link is error");
+    } else {
+        @autoreleasepool {
+            
+            
+            NSLog(@"Link is not error");
+            NSError *attributesError = nil;
+            NSDictionary *attributes = [fm attributesOfItemAtPath:  directoryPath  error:&attributesError];
+            unsigned long long sizeOfFile = [attributes fileSize];
+            unsigned long long sizeOfRestOfFileSize = sizeOfFile % NByte;
+            NSUInteger countFileHasTheSameByte = (sizeOfFile - sizeOfRestOfFileSize)/NByte;
+          
+            inputFileHandle = [NSFileHandle fileHandleForReadingAtPath: directoryPath];
+            [inputFileHandle seekToFileOffset: 0];
+            unsigned long long readCurrent = 0;
+            
+            for (NSInteger i = 1 ; i <= countFileHasTheSameByte + 1 ; i++){
+                
+                // create a file with size = sizeEachFileSplited
+                NSString *nameFile = [[[directoryPath lastPathComponent] stringByDeletingPathExtension] stringByAppendingString:[NSString stringWithFormat:@"(%tu)",i]];
+                NSString* aPathWithStringAddedIndex =[NSTemporaryDirectory() stringByAppendingString:nameFile];
+                NSLog(@"%@", aPathWithStringAddedIndex);
+                
+                [fm createFileAtPath:aPathWithStringAddedIndex contents: nil attributes: nil];
+                outputFileHandle = [NSFileHandle fileHandleForWritingAtPath:aPathWithStringAddedIndex];
+                [outputFileHandle seekToFileOffset: 0];
+                
+                NSData *inputDataBuffer;
+                if (readCurrent + NByte >  sizeOfFile ){
+                    inputDataBuffer = [inputFileHandle readDataToEndOfFile];
+                } else {
+                    inputDataBuffer = [inputFileHandle readDataOfLength:NByte];
+                }
+                
+                
+                [outputFileHandle writeData: inputDataBuffer];
+                NSLog(@"%@",inputDataBuffer);
+                
+                
+                
+                readCurrent = readCurrent + NByte;
+                [outputFileHandle closeFile];
+            }
+            [inputFileHandle closeFile];
+        }
+        
+    }
+    
+}
+-(void)splitFileWithURL:(NSURL *)urlInput andN:(NSUInteger)N {
+    NSFileHandle *inputFileHandle;
+    NSFileHandle *outputFileHandle;
+    NSFileManager * fm = [NSFileManager defaultManager];
+    NSString *directoryPath = urlInput.path;
+    NSLog(@"%@",directoryPath);
+    
+    if (![fm fileExistsAtPath:directoryPath]){
+        NSLog(@"Link is error");
+    } else {
+        //        @autoreleasepool {
+        
+        
+        NSLog(@"Link is not error");
+        NSError *attributesError = nil;
+        NSDictionary *attributes = [fm attributesOfItemAtPath:  directoryPath  error:&attributesError];
+        unsigned long long sizeOfFile = [attributes fileSize];
+        unsigned long long sizeEachFileSplited = sizeOfFile/N;
+        inputFileHandle = [NSFileHandle fileHandleForReadingAtPath: directoryPath];
+        [inputFileHandle seekToFileOffset: 0];
+        for (NSInteger i = 1 ; i <= N ; i++){
+            
+            // create a file with size = sizeEachFileSplited
+            NSString *nameFile = [[[directoryPath lastPathComponent] stringByDeletingPathExtension] stringByAppendingString:[NSString stringWithFormat:@"(%tu)",i]];
+            NSString* aPathWithStringAddedIndex =[NSTemporaryDirectory() stringByAppendingString:nameFile];
+            NSLog(@"%@", aPathWithStringAddedIndex);
+            
+            [fm createFileAtPath:aPathWithStringAddedIndex contents: nil attributes: nil];
+            outputFileHandle = [NSFileHandle fileHandleForWritingAtPath:aPathWithStringAddedIndex];
+            [outputFileHandle seekToFileOffset: 0];
+            
+            NSData *inputDataBuffer;
+            
+            
+            inputDataBuffer = [inputFileHandle readDataOfLength:sizeEachFileSplited];
+            [outputFileHandle writeData: inputDataBuffer];
+            NSLog(@"%@",inputDataBuffer);
+            
+            [outputFileHandle closeFile];
+        }
+        [inputFileHandle closeFile];
+    }
+    //    }
+    
+    
+    
+    
+}
+
+
+@end
