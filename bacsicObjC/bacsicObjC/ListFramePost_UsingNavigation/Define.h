ff75168defc6c87396f3447e2b72ff14e819de46
Thinh <hoaithipm2@gmail.com>
2019-08-26 15:38:28 +0700

    refactor code + fix bug uitableview

---
+++ bacsicObjC/bacsicObjC/ListFramePost_UsingNavigation/Define.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  bacsicObjC
+//
+//  Created by CPU11606 on 8/26/19.
+//  Copyright © 2019 CPU11606. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ bacsicObjC/bacsicObjC/ListFramePost_UsingNavigation/UITableView+SetEmptyView.h
@@ -0,0 +1,17 @@
+//
+//  UITableView+SetEmptyView.h
+//  bacsicObjC
+//
+//  Created by CPU11606 on 8/26/19.
+//  Copyright © 2019 CPU11606. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UITableView ()
+
+@end
+
+NS_ASSUME_NONNULL_END
