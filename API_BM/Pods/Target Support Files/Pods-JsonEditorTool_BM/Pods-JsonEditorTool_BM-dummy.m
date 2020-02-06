77f99494b8ddcb9c0c92dee9fbf08d7f416d36b6
hoaithi <mailthi216@gmail.com>
2020-02-06 11:20:59 +0700

    ad modul api

--- API_BM/Pods/Target Support Files/Pods-JsonEditorTool_BM/Pods-JsonEditorTool_BM-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JsonEditorTool_BM : NSObject
-@end
-@implementation PodsDummy_Pods_JsonEditorTool_BM
-@end
--- API_BM/Pods/Target Support Files/Pods-JsonEditorTool_BMTests/Pods-JsonEditorTool_BMTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JsonEditorTool_BMTests : NSObject
-@end
-@implementation PodsDummy_Pods_JsonEditorTool_BMTests
-@end
