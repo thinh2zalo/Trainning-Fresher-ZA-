948c0bbd765ecbb89901bb94de038223d5c8952c
hoaithi <mailthi216@gmail.com>
2020-02-06 11:38:02 +0700

    add new API

---
+++ API_JET/Pods/Target Support Files/Pods-JsonEditorTool_BM-JsonEditorTool_BMUITests/Pods-JsonEditorTool_BM-JsonEditorTool_BMUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JsonEditorTool_BM_JsonEditorTool_BMUITests : NSObject
+@end
+@implementation PodsDummy_Pods_JsonEditorTool_BM_JsonEditorTool_BMUITests
+@end
