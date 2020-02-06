fd1694ef11c89caaf55cf70badeb4b9a6a32a249
hoaithi <mailthi216@gmail.com>
2020-02-06 11:15:33 +0700

    remove

--- API_BM/Pods/Target Support Files/Pods-JsonEditorTool_BM-JsonEditorTool_BMUITests/Pods-JsonEditorTool_BM-JsonEditorTool_BMUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JsonEditorTool_BM_JsonEditorTool_BMUITests : NSObject
-@end
-@implementation PodsDummy_Pods_JsonEditorTool_BM_JsonEditorTool_BMUITests
-@end
