1f383f938f1fc8df8448ad020c22d4c991ac5378
hoaithi <mailthi216@gmail.com>
2020-02-24 16:58:24 +0700

    refactor code

---
+++ CalculatorLunarBM/CalculatorBM/BMDate+Utility.h
@@ -0,0 +1,20 @@
+//
+//  BMDate+Utility.h
+//  CalculatorBM
+//
+//  Created by vincent on 2/21/20.
+//  Copyright © 2020 vincent. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import "BMDate.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BMDate (Utility)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CalculatorLunarBM/CalculatorBM/BMDate+Utility.m
@@ -0,0 +1,16 @@
+//
+//  BMDate+Utility.m
+//  CalculatorBM
+//
+//  Created by vincent on 2/21/20.
+//  Copyright © 2020 vincent. All rights reserved.
+//
+
+#import "BMDate+Utility.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation BMDate (Utility)
+
+@end
