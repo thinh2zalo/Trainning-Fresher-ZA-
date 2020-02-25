bbc675ced2b340f9667e14c4e2a06488e7ff67a8
hoaithi <mailthi216@gmail.com>
2020-02-25 22:45:37 +0700

    implement BMPaging

---
+++ CalculatorLunarBM/CalculatorBM/BMPageViewController.h
@@ -0,0 +1,17 @@
+//
+//  BMPageViewController.h
+//  CalculatorBM
+//
+//  Created by vincent on 2/25/20.
+//  Copyright © 2020 vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BMPageViewController : UIPageViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CalculatorLunarBM/CalculatorBM/BMPageViewController.m
@@ -0,0 +1,32 @@
+//
+//  BMPageViewController.m
+//  CalculatorBM
+//
+//  Created by vincent on 2/25/20.
+//  Copyright © 2020 vincent. All rights reserved.
+//
+
+#import "BMPageViewController.h"
+
+@interface BMPageViewController ()
+
+@end
+
+@implementation BMPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
