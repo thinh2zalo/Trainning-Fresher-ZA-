b3210f33458c6528ffb7a4fa37c3739fefc1492b
hoaithi <mailthi216@gmail.com>
2020-04-01 21:06:37 +0700

    fix bug + refactor code

---
+++ ThreadSafe/ThreadSafe/GCDLock.swift
@@ -0,0 +1,13 @@
+//
+//  GCDLock.swift
+//  ThreadSafe
+//
+//  Created by vincent on 4/1/20.
+//  Copyright © 2020 vincent. All rights reserved.
+//
+
+import UIKit
+
+class GCDLock: NSLocking {
+
+}
